d60557e271095a8ae526cad2e5103d48259fd901
BorislavTserovski <Borislav.Tserovski@gmail.com>
2016-12-13 17:19:56 +0200

    Add files via upload

---
+++ Models/Recipe.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCBlog.Models
+{
+    public class Recipe
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        [DisplayName("Name")]
+        public string Title { get; set; }
+        [DisplayName("Recipe")]
+        [Required]
+        public string Body { get; set; }
+
+        [Required]
+        [DisplayName("Date Added")]
+        [DisplayFormat(DataFormatString = "{0:d}")]
+        public DateTime Date { get; set; }
+
+        
+        public ApplicationUser Author { get; set;}
+    }
+}
