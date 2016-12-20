6415fe657480a4938412336e8c779ea92a0cee54
BorislavTserovski <Borislav.Tserovski@gmail.com>
2016-12-20 19:35:16 +0200

    Add files via upload

---
+++ dbo.Recipes.data.sql
@@ -0,0 +1,3 @@
+﻿SET IDENTITY_INSERT [dbo].[Recipes] ON
+INSERT INTO [dbo].[Recipes] ([Id], [Title], [Body], [Date], [AuthorId], [Image], [CategoryId], [Category_Id]) VALUES (7, N'Cosmopolitan', N'Vodka, Triple Sec, Cranberry Juice', N'2016-12-21 00:00:00', N'53f802fc-fb52-47af-a5b6-03517ce2643d', <Binary Data>, 1, NULL)
+SET IDENTITY_INSERT [dbo].[Recipes] OFF
