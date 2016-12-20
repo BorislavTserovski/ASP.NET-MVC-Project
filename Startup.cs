c349cd19d8677126f6f2a66c4ec30dd888aea076
BorislavTserovski <Borislav.Tserovski@gmail.com>
2016-12-20 19:34:25 +0200

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Owin;
+using Owin;
+using MVCBlog.Migrations;
+using MVCBlog.Models;
+using System.Data.Entity;
+
+
+[assembly: OwinStartupAttribute(typeof(MVCBlog.Startup))]
+namespace MVCBlog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            Database.SetInitializer(
+                new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
+            ConfigureAuth(app);
+        }
+    }
+}
